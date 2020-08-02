6b3ccfc91a8a85c03817dbab7895baf4df858db9
ppsobralwb <ppsobralwb@gmail.com>
2020-08-02 01:23:14 -0300

    agora foi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Menu from './components/Menu'
+import Carousel from './components/Carousel'
+import dadosIniciais from './data/dados_iniciais'
+import Footer from './components/Footer'
+import BannerMain from './components/BannerMain'
 
 function App() {
   return (
     <div>
       <Menu />
+      <BannerMain 
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+      videoDescription={"Escreva aqui a sua descrição"}
+      url={dadosIniciais.categorias[0].videos[0].url}/>
+      <Carousel
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[0]}
+      />
+      <Carousel
+      category={dadosIniciais.categorias[1]}
+      />
+      <Footer/>
     </div>
   );
 }
